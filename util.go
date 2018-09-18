31f298be83e4221d940bd2cf0a246f173074290e
Beyang Liu <beyang@sourcegraph.com>
2018-09-17 21:33:00 -0700

    refactor

--- util.go
+++ util.go
@@ -3,6 +3,7 @@ package kg
 import (
 	"io/ioutil"
 	"log"
+	"reflect"
 
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
@@ -27,10 +28,41 @@ func IntstrPtr(v intstr.IntOrString) *intstr.IntOrString {
 	return &v
 }
 
-func ReadFile(filename string) string {
+func ReadString(filename string) string {
 	b, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatalf("Could not read file %s: %v", filename, err)
 	}
 	return string(b)
+}
+
+func ReadYAML(filename string) map[string]interface{} {
+
+}
+
+// Merge merges the from map into into
+func Merge(into map[string]interface{}, from map[string]interface{}) {
+	mergeReflect(reflect.ValueOf(into), reflect.ValueOf(from))
+}
+
+func mergeReflect(into reflect.Value, from reflect.Value) {
+	if into.Type().Kind() != reflect.Map || from.Type().Kind() != reflect.Map {
+		return
+	}
+
+	if !into.IsValid() || !from.IsValid() || into.IsNil() || from.IsNil() {
+		return
+	}
+
+	for _, k := range from.MapKeys() {
+		fromV := from.MapIndex(k)
+		intoV := into.MapIndex(k)
+		if !intoV.IsValid() || intoV.IsNil() || intoV.Type().Kind() != reflect.Map || fromV.Type().Kind() != reflect.Map {
+			// Overwrite if not map-to-map
+			into.SetMapIndex(k, fromV)
+			continue
+		}
+		// Map-to-map merge, so recurse
+		mergeReflect(intoV, fromV)
+	}
 }
